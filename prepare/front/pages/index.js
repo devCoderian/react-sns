ecae0d8304c3028b50938006c4665c88787cc380
devCoderian <devCoderian@gmail.com>
2021-09-15 22:13:49 +0900

    add postform

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,11 +1,22 @@
 import AppLayout from "../components/AppLayout";
+import { useSelector } from 'react-redux';
 
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
 const Home = () => {
-    return(
-        <AppLayout>
-            <div>Hello,Next!</div>
-        </AppLayout>
-    )
-}
+    const { isLoggedIn } = useSelector(state => state.user);
+    const { mainPosts } = useSelector(state => state.post);
+
+  return (
+    <AppLayout>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => {
+        return (
+          <PostCard key={post.id} post={post} />
+        );
+      })}
+    </AppLayout>
+  );
+};
 
 export default Home;

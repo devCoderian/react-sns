98730d5707989b551ce6bcc0f24988a9282bfa96
devCoderian <devcoderian@gmail.com>
2021-12-06 23:44:05 +0900

    saga backurl

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -2,10 +2,10 @@ import { all, fork } from 'redux-saga/effects'
 import postSaga from './post';
 import userSaga from './user';
 
-
 import axios from 'axios';
+import { backUrl } from '../config/config';
 // axios.defaults.baseURL = 'http://localhost:3060';
-axios.defaults.baseURL = 'http://3.36.64.46/';
+axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 export default function* rootSaga(){
     yield all([

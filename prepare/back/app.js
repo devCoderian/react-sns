b4a4761ac4b9933503dd16cda331bc7bc469c7cc
devCoderian <devcoderian@gmail.com>
2021-12-07 15:25:58 +0900

    도메인 추가

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -82,7 +82,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: false,
-        domain: process.env.NODE_ENV === 'production' && '.nodebird.com'
+        domain: process.env.NODE_ENV === 'production' && '.reactsnspf.p-e.kr'
     }
 }));
 app.use(passport.initialize()); 
